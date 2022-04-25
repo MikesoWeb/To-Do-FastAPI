3bc8b00ecd5b13c571997fb7a5d03645d56d7726
MikesoWeb <klionpsyx@gmail.com>
2022-04-25 17:26:36 +0300

    'commit_init'

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from database import Base
+
+
+class ToDo(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    is_complete = Column(Boolean, default=False)
