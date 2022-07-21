fdb4ab44e002ba5dc753f672c78086b87b91945f
MikesoWeb <klionpsyx@gmail.com>
2022-07-21 16:16:13 +0300

    'chenge_files'

--- todo/config.py
+++ todo/config.py
@@ -10,7 +10,6 @@ class Settings(BaseSettings):
     app_name = os.getenv('NAME_APP')
     db_sqlite_url = os.getenv('SQLALCHEMY_DATABASE_URI')
     db_postgre_url = os.getenv('POSTGRES_DB')
-    port = os.getenv("PORT", default=5000)
 
     class Config:
         env_file: str = '../.env'
