0f6d9f7ef079caaf9bdac1191cc05ef39da3b895
MikesoWeb <klionpsyx@gmail.com>
2022-07-21 16:43:46 +0300

    'change_files'

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 
 import uvicorn
 from todo.app import app
-from todo.config import settings
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
